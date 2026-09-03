f3aea5df15ba1aa93793c9d60fdd6d596ebe904a
Deividi Luccas <deividiluccas@outlook.com>
2026-09-03 19:44:02 -0300

    Adiciona suporte a DATABASE_URL para deploy

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import os
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
@@ -6,35 +7,20 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-# Produção / Neon
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Banco online: Neon / Render
 if DATABASE_URL:
-
-    # Compatibilidade com URLs fornecidas por serviços PostgreSQL
-    if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace(
-            "postgres://",
-            "postgresql+psycopg2://",
-            1
-        )
-
-    elif DATABASE_URL.startswith("postgresql://"):
-        DATABASE_URL = DATABASE_URL.replace(
-            "postgresql://",
-            "postgresql+psycopg2://",
-            1
-        )
 
     engine = create_engine(
         DATABASE_URL,
         pool_pre_ping=True
     )
 
+# Banco local
 else:
 
-    # Desenvolvimento local
-    DATABASE_URL = URL.create(
+    LOCAL_DATABASE_URL = URL.create(
         drivername="postgresql+psycopg2",
         username=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
@@ -44,7 +30,7 @@ else:
     )
 
     engine = create_engine(
-        DATABASE_URL,
+        LOCAL_DATABASE_URL,
         pool_pre_ping=True
     )
 
